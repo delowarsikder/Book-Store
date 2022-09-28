faa1cea367ea3448629cb4486fb114b19c3b6dc3
delowarsikder <delowarsikder099@gmail.com>
2022-09-28 16:18:36 +0600

    check out cart item

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,11 +1,3 @@
-#---
-# Excerpted from "Agile Web Development with Rails 6",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material,
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose.
-# Visit http://www.pragmaticprogrammer.com/titles/rails6 for more book information.
-#---
 class CartsController < ApplicationController
   before_action :set_cart, only: %i[show edit update destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
@@ -88,3 +80,4 @@ class CartsController < ApplicationController
       redirect_to store_index_url, notice: 'Invalid cart'
     end
 end
+
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ApplicationRecord
   has_many :line_items ,dependent: :destroy
   before_destroy :ensure_not_referenced_by_any_line_item
-
+  has_many :orders, through: :line_items
+  
   validates :title,:description,:price, presence:true
   validates :title ,uniqueness: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
